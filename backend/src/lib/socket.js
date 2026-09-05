0021b774f4207f3133f87e212ce354d757c4a241
Ayush Anand <ayushanand10521@gmail.com>
2026-09-05 11:26:06 +0530

    fix: socket disconnect race condition

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -29,18 +29,17 @@ io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
 
     const userId = socket.handshake.query?.userId;
-    if (userId) {
+    if (userId && userId !== "undefined") {
         userSocketMap[userId] = socket.id;
+        io.emit("onlineUsers", Object.keys(userSocketMap));
     }
-
-    io.emit("onlineUsers", Object.keys(userSocketMap));
 
     socket.on("disconnect", () => {
         console.log("User disconnected:", socket.id);
-        if (userId) {
+        if (userId && userSocketMap[userId] === socket.id) {
             delete userSocketMap[userId];
+            io.emit("onlineUsers", Object.keys(userSocketMap));
         }
-        io.emit("onlineUsers", Object.keys(userSocketMap));
     });
 });
 
